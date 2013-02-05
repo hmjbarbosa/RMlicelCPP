1b1fb8f6645daa1e5663cc9a1a61526d9331cd86
Henrique Barbosa <hmjbarbosa@gmail.com>
2013-02-05 15:07:50 -0200

    hearder for molecular.m and some more cleaning of the code.

--- leipzig_birgit/molecular.m
+++ leipzig_birgit/molecular.m
@@ -1,6 +1,49 @@
 %------------------------------------------------------------------------
-% molecular.m
+% M-File:
+%    molecular.m
 %
+% Authors:
+%    H.M.J. Barbosa (hbarbosa@if.usp.br), IF, USP, Brazil
+%    B. Barja (bbarja@gmail.com), GOAC, CMC, Cuba
+%    R. Costa (re.dacosta@gmail.com), IPEN, Brazil
+%
+% Description
+%
+%    Takes temperature and pressure from the reference sounding and
+%    calculates the refractive index, king's correction factor,
+%    depolarization ratio, molecular phase function and cross-section
+%    and, finally, molecular backscatter and extinction coefficients.
+%
+% Input
+%
+%    nlev_snd - number of levels in sounding
+%    pres_snd(nlev_snd, 1) - column with pressure in hPa
+%    temp_snd(nlev_snd, 1) - column with temperature in K
+%
+% Ouput
+%
+%    lambda   (1, 2) - wavelengths [microns]
+%    nAir     (1, 2) - index of refraction []
+%    fAir     (1, 2) - king factor of air []
+%    rhoAir   (1, 2) - depolarization factor of air []
+%    P_mol    (1, 2) - phase function at -180deg []
+%    LR_mol   (1, 2) - molecular lidar ratio [sr]
+%
+%    alpha_mol_snd(nlev_snd, 2) - extinction coefficient [km^-1]
+%    beta_mol_snd (nlev_snd, 2) - backscatter coefficient [km^-1 sr^-1]
+%
+% Usage
+%
+%    First run: 
+%
+%        read_sonde_*.m
+%
+%    This will set nlev_snd, pres_snd and temp_snd, the number of
+%    levels in the souding data, the pressure and temperature in each
+%    level, respectively. User must also change the code to set co2
+%    concentration and the elastic and raman wavelengths.
+%
+% References
 % 
 %    Bates, 1984: Planet. Space Sci., v. 32, p. 785
 %    Bodhaine et al, 1999: J. Atmos. Ocea. Tech, v. 16, p.1854
@@ -10,11 +53,6 @@
 %    McCartney, E. J., 1976: Optics of the Atmosphere. Wiley, 408 pp.
 %    Peck and Reeder, 1973: J. Opt. Soc. Ame., v 62 (8), p. 958
 %
-%------------------------------------------------------------------------
-% first run the following programs:
-%   
-%    read_ascii_*.m
-%    read_sonde_*.m
 %------------------------------------------------------------------------
 
 % Read Physics Constants
@@ -28,6 +66,7 @@ lambda_raman=0.387;    % Raman N2 [microns]
 co2ppmv = 375; % CO2 concentration [ppmv]
 
 lambda=[lambda_rayleigh lambda_raman];
+clear lambda_rayleigh lambda_raman;
 
 %%------------------------------------------------------------------------
 %% REFRACTIVE INDEX WITH CO2 CORRECTION 
@@ -47,12 +86,13 @@ else
 	14455.7 ./ (39.32957 - 1./lambda.^2))*1e-8;
 end
 
-%% Correct for different concentration
+%% Correct for different concentration of CO2
 %% Bodhaine et al (1999), Eq (19)
 dnAir = dn300 .* (1 + (0.54 * (co2ppmv*1e-6 - 0.0003)));
 
 %% Actual index of refraction at 300 ppmv CO2
 nAir = 1 + dnAir;
+clear dnAir;
 
 %%------------------------------------------------------------------------
 %% KING FACTOR AND DEPOLARIZATION RATIO
@@ -89,6 +129,7 @@ fAir = (N2ppv*fN2 + O2ppv*fO2 + Arppv*fAr + co2ppmv*1e-6*fCO2)./...
 %
 % hmjb - What is the correct way?
 rhoAir = (6*fAir-6)./(3+7*fAir);
+clear fN2 fO2 fAr fCO2;
 
 %%------------------------------------------------------------------------
 %% RAYLEIGH PHASE FUNCTION AT 180deg
@@ -97,7 +138,7 @@ rhoAir = (6*fAir-6)./(3+7*fAir);
 % Chandrasekhar (Chap. 1, p. 49)
 % or Bucholtz (1995), eqs (12) and (13)
 gammaAir = rhoAir./(2-rhoAir);
-P_Ray = 0.75*((1+3.*gammaAir)+(1-gammaAir)*(cos(pi)^2))./(1+2.*gammaAir);
+P_mol = 0.75*((1+3.*gammaAir)+(1-gammaAir)*(cos(pi)^2))./(1+2.*gammaAir);
 
 %%------------------------------------------------------------------------
 %% RAYLEIGH TOTAL SCATERING CROSS SECTION
@@ -105,7 +146,7 @@ P_Ray = 0.75*((1+3.*gammaAir)+(1-gammaAir)*(cos(pi)^2))./(1+2.*gammaAir);
 
 % McCartney (1976)
 % units: lambda*1e-6 [m], Nstd*1e6 [#/m^3]
-% hence sigma_s is in [m2]
+% hence sigma_std is in [m2]
 sigma_std = 24 * (pi^3) * ((nAir.^2-1).^2) .* fAir ./...
     ( ((lambda*1e-6).^4) .* ((Nstd*1e6)^2) .* (((nAir.^2)+2).^2) );
 
@@ -121,9 +162,11 @@ sigma_std = 24 * (pi^3) * ((nAir.^2-1).^2) .* fAir ./...
 % Bucholtz (1995), eq (9), units [km^-1]
 % factor 1e6 converts #/cm^3 to #/m^3
 alpha_std = Nstd*1e6 * sigma_std * 1000.; 
+clear sigma_std;
 % Bucholtz (1995), eq (10), units [km^-1]
 % scaling for each P and T in the column 
 alpha_mol_snd = ((pres_snd./temp_snd) * Tstd/Pstd) * alpha_std;
+clear alpha_std;
 
 %%------------------------------------------------------------------------
 %% RAYLEIGH ANGULAR VOLUME-SCATTERING COEFFICIENT
@@ -131,7 +174,7 @@ alpha_mol_snd = ((pres_snd./temp_snd) * Tstd/Pstd) * alpha_std;
 
 % Rayleigh extinction to backscatter ratio
 % ie, Rayleigh lidar ratio [sr]
-LR_mol = (4*pi)./P_Ray; 
+LR_mol = (4*pi)./P_mol; 
 
 % In traditional lidar notation, Bucholtz (1995) eq (14) defines the
 % backscattering coeficient. Here the usual greek letter 'beta' is
@@ -139,6 +182,7 @@ LR_mol = (4*pi)./P_Ray;
 
 % Multiply by phase function for -180deg and divide by 4pi steradians 
 % Units: [km]-1 [sr]-1
-beta_mol_snd = alpha_mol_snd*diag(P_Ray)/(4*pi); 
+%beta_mol_snd = alpha_mol_snd*diag(P_mol)/(4*pi); 
+beta_mol_snd = alpha_mol_snd*diag(LR_mol.^-1); 
 
 %
