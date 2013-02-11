8bac53da900d3d9745c2f62e37ecea702749cbe6
Henrique Barbosa <hmjbarbosa@gmail.com>
2013-02-11 12:32:35 -0200

    improved version of rayleigh_fit. still a lot to do.

--- leipzig_birgit/rayleigh_fit_Manaus.m
+++ leipzig_birgit/rayleigh_fit_Manaus.m
@@ -9,7 +9,8 @@
 %       read_ascii_Manaus.m 
 %       read_sonde_Manaus.m
 % ---------------------------------------------------
-clear altbin xl_scal_1 xu_scal_1 xl_scal_2 xu_scal_2 xl_scal_3 xu_scal_3
+clear maxbin beta_mol alpha_mol
+clear xl_scal_1 xu_scal_1 xl_scal_2 xu_scal_2 xl_scal_3 xu_scal_3
 clear meanRaySig meanPr2 SigFak RaySig log_Pr2 Ray_Fit
 clear RefBin diff_1 abst_1 diff_2 abst_2 diff_3 abst_3
 % ---------------------------------------------------
@@ -18,63 +19,83 @@ clear RefBin diff_1 abst_1 diff_2 abst_2 diff_3 abst_3
 %%  INTERPOLATION TO LIDAR SAMPLING ALTITUDES
 %%------------------------------------------------------------------------
 
-% highest altitude of sounding in units of lidar bins
-maxbin=floor(alt_snd(nlev_snd)*1e-3/r_bin);
-% or if you want to extrapolate the sounding data, just set to the
-% number of bins in the lidar data
-%maxbin = size(channel,1);
+toextrapolate=0;
 
-% hmjb - beta_ray decays exponentially towards zero with increasing
-% height. Extrapolating it above the highest level in the sounding can
-% lead to negative (unphysical) values. Hence interpolation is done in
-% log() and then the exp() of the result is taken.
-beta_mol(:,1) = exp(interp1(alt_snd, log(beta_mol_snd(:,1)), alt(1:maxbin),'linear','extrap'));
-beta_mol(:,2) = exp(interp1(alt_snd, log(beta_mol_snd(:,2)), alt(1:maxbin),'linear','extrap'));
-%beta_mol(1,:) = exp(interp1(alt_snd, log(beta_mol_snd(:,1)), alt(1:maxbin),'linear','extrap'));
-%beta_mol(2,:) = exp(interp1(alt_snd, log(beta_mol_snd(:,2)), alt(1:maxbin),'linear','extrap'));
+if toextrapolate==0
+  % Set maximum lidar bin to highest altitude of sounding 
+  maxbin=floor(alt_snd(nlev_snd)*1e-3/r_bin);
 
-% -----------------
-%  Rayleigh Signal 
-% -----------------
+  % Simple linear interpolation within the souding range
+  beta_mol(:,1) = interp1(alt_snd, beta_mol_snd(:,1), ... 
+			      alt(1:maxbin),'linear','extrap');
+  beta_mol(:,2) = interp1(alt_snd, beta_mol_snd(:,2), ...
+			      alt(1:maxbin),'linear','extrap');
+else
+  % Set maximum lidar bin to total number of bins
+  maxbin = size(Pr2,1);
+
+  % Beta_mol() decays exponentially towards zero with increasing
+  % height. Extrapolating it above the highest level in the sounding can
+  % lead to negative (unphysical) values. Hence interpolation is done in
+  % log() and then the exp() of the result is taken.
+  beta_mol(:,1) = exp(interp1(alt_snd, log(beta_mol_snd(:,1)), ... 
+			      alt(1:maxbin),'linear','extrap'));
+  beta_mol(:,2) = exp(interp1(alt_snd, log(beta_mol_snd(:,2)), ...
+			      alt(1:maxbin),'linear','extrap'));
+end
+
+%%------------------------------------------------------------------------
+%%  RAYLEIGH SIGNAL 
+%%------------------------------------------------------------------------
+
+% Instead of interpolating alpha_mol_snd, it is more precise to
+% calculate it again using the molecular lidar ratio
 alpha_mol(:,1) = beta_mol(:,1).*LR_mol(1); 
 alpha_mol(:,2) = beta_mol(:,2).*LR_mol(2); 
-% 
+
+% Compute molecular optical depth between lidar height and each
+% atmospheric level
 for j = 1:2
   for i=1:maxbin
     if i==1
-      tau(j,i) = alpha_mol(i,j)*r_bin; 
+      tau(i,j) = alpha_mol(i,j)*r_bin; 
     else
-      tau(j,i) = tau(j,i-1)+alpha_mol(i,j)*r_bin; 
+%hmjb try to use trapezium rule
+%      tau(j,i) = tau(j,i-1)+alpha_mol(i,j)*r_bin; 
+      tau(i,j) = tau(i-1,j)+(alpha_mol(i,j)+alpha_mol(i,j))*r_bin/2; 
     end
   end
 end
-
+% Compute "expected" molecular signal. Because it is not divided by
+% z^2 this is already range corrected. Note that units are completely
+% arbitrary: there is no detector efficiency, no mirror, not even the
+% raman cross section! Beta_mol() however, is proportional to the
+% nitrogen number density, and hence, proportional to the N2-raman
+% back-scatter.
 for j = 1:2
   for i=1:maxbin
-    % calculate Pr2_ray_sig in km-1 
-    if (j==1 || j==2)
-      Pr2_ray_sig(j,i)=beta_mol(i,j)*exp(-tau(j,i)-tau(1,i));
-    elseif (j==3 || j==4)
-      Pr2_ray_sig(j,i)=beta_mol(i,j)*exp(-tau(j,i)-tau(3,i));
-    else
-      Pr2_ray_sig(j,i)=beta_mol(i,j)*exp(-tau(j,i)-tau(5,i));
-    end
+    % calculate Pr2_mol in km-1 
+    Pr2_mol(i,j)=beta_mol(i,j)*exp(-tau(i,j)-tau(i,1));
   end
 end
 
+%%------------------------------------------------------------------------
+%% 
+%%------------------------------------------------------------------------
 
 % ----------------------------------------
 %  choose altitude range for Rayleigh fit
 % ----------------------------------------
-% 355 nm elastic channel
+% elastic channel
 xl_scal_1 = round(15/r_bin); % km
 xu_scal_1 = round(17/r_bin); % km
-% 387 nm Raman channel
+% Raman channel
 xl_scal_2 = round(15/r_bin); % km
 xu_scal_2 = round(17/r_bin); % km
+
 % simulado molecular
-meanRaySig(1) = mean(Pr2_ray_sig(1,xl_scal_1:xu_scal_1)); 
-meanRaySig(2) = mean(Pr2_ray_sig(2,xl_scal_2:xu_scal_2)); 
+meanRaySig(1) = mean(Pr2_mol(xl_scal_1:xu_scal_1,1)); 
+meanRaySig(2) = mean(Pr2_mol(xl_scal_2:xu_scal_2,2)); 
 % 
 % ----------------
 %   mean signals 
@@ -84,15 +105,35 @@ meanPr2(1) = mean(Pr2(xl_scal_1:xu_scal_1,1));
 meanPr2(2) = mean(Pr2(xl_scal_2:xu_scal_2,2)); 
 %    
 % Scales the molecular-sounding to the "values" of the lidar data
-RaySig(1,:) = Pr2_ray_sig(1,:)*meanPr2(1)/meanRaySig(1); 
-RaySig(2,:) = Pr2_ray_sig(2,:)*meanPr2(2)/meanRaySig(2); 
+RaySig(:,1) = Pr2_mol(:,1)*meanPr2(1)/meanRaySig(1); 
+RaySig(:,2) = Pr2_mol(:,2)*meanPr2(2)/meanRaySig(2); 
+
+figure(20)
+plot(Pr2(xl_scal_1:xu_scal_1,1),'ro');
+hold on;
+plot(RaySig(xl_scal_1:xu_scal_1,1),'b');
+hold off;
+
 % ------------
 %  logarithm
 % ------------
-Ray_Fit(1,:) = log(RaySig(1,:));  
-Ray_Fit(2,:) = log(RaySig(2,:)); 
-            
+Ray_Fit(1,:) = log(RaySig(:,1));  
+Ray_Fit(2,:) = log(RaySig(:,2)); 
 log_Pr2 = real(log(Pr2));    
+
+figure(21)
+plot(log_Pr2(xl_scal_1:xu_scal_1,1),'ro');
+hold on;
+plot(Ray_Fit(1,xl_scal_1:xu_scal_1),'b');
+hold off;
+
+figure(22)
+plot(Pr2_mol(xl_scal_1:xu_scal_1,1),Pr2(xl_scal_1:xu_scal_1,1),'ro');
+f0=fit(Pr2_mol(xl_scal_1:xu_scal_1,1),Pr2(xl_scal_1:xu_scal_1,1),'poly1');
+hold on;
+plot(Pr2_mol(xl_scal_1:xu_scal_1,1),RaySig(xl_scal_1:xu_scal_1,1),'b');
+plot(f0,'g');
+
 %        
 % ----------------------
 %   find reference bins
@@ -160,7 +201,7 @@ ylabel('height / km','fontsize',12)
 title('Rayleigh Fit 355','fontsize',14)
 grid on
 hold on
-plot(RaySig(1,1:maxbin), alt(1:maxbin)*1e-3,'g','LineWidth',2); 
+plot(RaySig(1:maxbin,1), alt(1:maxbin)*1e-3,'g','LineWidth',2); 
 plot(Pr2(RefBin(1),1), alt(RefBin(1))*1e-3,'r*');
 hold off
 legend('Lidar', 'Rayleigh Fit', 'Reference Bin'); 
@@ -171,7 +212,7 @@ xlabel('range smooth bg-corr signal','fontsize',[10])
 title('Rayleigh Fit 387','fontsize',14)
 grid on
 hold on
-plot(RaySig(2,1:maxbin), alt(1:maxbin)*1e-3,'g','LineWidth',2); 
+plot(RaySig(1:maxbin,2), alt(1:maxbin)*1e-3,'g','LineWidth',2); 
 plot(Pr2(RefBin(2),2), alt(RefBin(2))*1e-3,'r*');
 legend('Lidar', 'Rayleigh Fit', 'Reference Bin'); 
 %
