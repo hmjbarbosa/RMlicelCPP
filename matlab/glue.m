2b21404c65031622dd73fc9c2a2be97d514a59d7
Henrique de Melo Jorge Barbosa <hbarbosa@floresta.if.usp.br>
2013-02-20 09:42:15 -0300

    Previous version of glue had a serius bug: it was using the information of channel #3 to do the glueing (compute the analog channel resolution) instead of the current channel. The confusion araise when passing the full header (all channels) instead of only the info for the channels being glued. With the new version of profile_read.m (last commit), this is now possible.

--- matlab/glue.m
+++ matlab/glue.m
@@ -1,16 +1,16 @@
-function [glued] = glue(analog, photon, header, toplot)
+function [glued] = glue(anSignal, anChannel, pcSignal, pcChannel, toplot)
 
-[nz nt] = size(analog);
+[nz nt] = size(anSignal);
 
 %% GLUE ALL TIMES
 for t=1:nt
-
   if exist('toplot','var')
-    glued(:,t)=glue_single(analog(:,t), photon(:,t), header, toplot);
+    glued(:,t)=glue_single(anSignal(:,t), anChannel, ...
+                           pcSignal(:,t), pcChannel, toplot);
   else
-    glued(:,t)=glue_single(analog(:,t), photon(:,t), header);
+    glued(:,t)=glue_single(anSignal(:,t), anChannel, ...
+                           pcSignal(:,t), pcChannel);
   end
-
 end
 
 %
--- matlab/glue_single.m
+++ matlab/glue_single.m
@@ -1,24 +1,27 @@
-function [glued] = glue_single(analog, photon, header, toplot)
+function [glued] = glue_single(anSignal, anChannel, pcSignal, pcChannel, toplot)
 
-N=ndims(analog);
+N=ndims(anSignal);
 if (N>1)
-  [nx ny] = size(analog);
+  [nx ny] = size(anSignal);
   if (ny>1 || N>2)
     error('Only 1-dim to glue!')
   end
 end
 
 % Length of data
-n=header.ndata(3);
+n=anChannel.ndata;
 idx=1:n;
 
 % Resolution (mV) of analog channel)
-resol=header.discr(3)/2^header.bits(3);
+resol=anChannel.discr/2^anChannel.bits;
+if exist('toplot','var')
+  disp(['resol= ' num2str(resol)]);
+end
 
 % Create a mask for the region where analog and PC are thought to be
 % proportional: below 7MHZ and above 5*resolution
-mask=(analog>5*resol) & (analog>0.) & (analog~=NaN) & ...
-     (photon<7.)      & (photon>0.) & (photon~=NaN);
+mask=(anSignal>5*resol) & (anSignal>0.) & (anSignal~=NaN) & ...
+     (pcSignal<7.)      & (pcSignal>0.) & (pcSignal~=NaN);
 %mask=(idx>1000) & (idx<1500.);
 
 % limits of fit region. result of min() or max() is an array with the
@@ -32,52 +35,58 @@ end
 % check if there was something different from NaN
 % in this case, the size of max/min vectors should be larger than one
 if ~(numel(idxmin) & numel(idxmax))
+  if exist('toplot','var')
+    disp(['error #1']);
+  end  
   glued(1:n)=NaN;
   return;
 end
 % take only a continuous mask, with no 0s in between
-for i=idxmin:idxmax
-  if ~mask(i)
-    mask(i:idxmax)=0;
-    idxmax=i-1;
-    break;
-  end
-end
+%for i=idxmin:idxmax
+%  if ~mask(i)
+%    mask(i:idxmax)=0;
+%    idxmax=i-1;
+%    break;
+%  end
+%end
 % check if there is enough points
-if (idxmax-idxmin<10)
+if (idxmax-idxmin<10 || sum(mask)<10)
   glued(1:n)=NaN;
+  if exist('toplot','var')
+    disp(['error #2']);
+  end  
   return;
 end
 
 % Do a linear fit between both channels
 if exist('toplot','var')
-  [cfun]=fit(analog(mask),photon(mask),'poly1');
+  [cfun]=fit(anSignal(mask),pcSignal(mask),'poly1');
 end
-[a, b]=fastfit(analog(mask),photon(mask));
+[a, b]=fastfit(anSignal(mask),pcSignal(mask));
 
 % glue vectors
 ig=floor((idxmax+idxmin)/2);
-% glued(1:ig)=cfun(analog(1:ig));
-glued(1:ig)=bsxfun(@plus, bsxfun(@times,analog(1:ig),a), b);
-glued(ig+1:n)=photon(ig+1:n);
+% glued(1:ig)=cfun(anSignal(1:ig));
+glued(1:ig)=bsxfun(@plus, bsxfun(@times,anSignal(1:ig),a), b);
+glued(ig+1:n)=pcSignal(ig+1:n);
 glued=glued';
 
 % Plot glue function 
 if exist('toplot','var')
-  plot(cfun,'m',analog(mask), photon(mask),'o');
+  plot(cfun,'m',anSignal(mask), pcSignal(mask),'o');
   title('Linear fit for glueing');
-  xlabel('Analog (mV)');
-  ylabel('PC (MHz)');
+  xlabel('anSignal (mV)');
+  ylabel('pcSignal (MHz)');
   grid on;
   cfun
   pause;
 
   % Plot glued and PC
-  semilogy(idx(1:3000),photon(1:3000),'r');
+  semilogy(idx(1:3000),pcSignal(1:3000),'r');
   hold on;
   semilogy(idx(1:3000),glued(1:3000),'b');
-  semilogy(idx(mask),photon(mask),'.g');
-  semilogy(idx(1:3000),analog(1:3000),'.m');
+  semilogy(idx(mask),pcSignal(mask),'.g');
+  semilogy(idx(1:3000),anSignal(1:3000),'.m');
   legend('Uncorrected PC','Scaled Analog','fit region','analog');
   xlabel('#bins');
   ylabel('PC (MHz)');
