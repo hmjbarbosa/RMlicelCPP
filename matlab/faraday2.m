e163ebdd6f9d223c2d1f98612845b7c2c8bd4162
Henrique Barbosa <hmjbarbosa@gmail.com>
2013-03-24 13:10:08 -0300

    new graphics and analysis routines for faraday...

--- matlab/faraday2.m
+++ matlab/faraday2.m
@@ -34,9 +34,43 @@ datain='/home/lidar_data/data';
 %jdi=datenum('26-Jan-2012 00:10:45'); jdf=datenum('26-Jan-2012 01:41:07')
 %jdi=datenum('26-Jan-2012 02:33:21'); jdf=datenum('26-Jan-2012 02:58:24')
 %jdi=datenum('26-Jan-2012 16:03:42'); jdf=datenum('26-Jan-2012 16:51:21')
+%% DRY NO CLOUDS
+%jdi=datenum('29-Jul-2011 06:31:30'); jdf=datenum('29-Jul-2011 08:19:47')
+%jdi=datenum('29-Jul-2011 17:30:26'); jdf=datenum('29-Jul-2011 18:00:05')
+%jdi=datenum('29-Jul-2011 18:00:37'); jdf=datenum('29-Jul-2011 22:06:01')
+%jdi=datenum('30-Jul-2011 06:36:51'); jdf=datenum('30-Jul-2011 08:05:22')
+%jdi=datenum('30-Jul-2011 09:23:12'); jdf=datenum('30-Jul-2011 11:03:40')
+%jdi=datenum('30-Jul-2011 14:15:40'); jdf=datenum('30-Jul-2011 14:39:33')
+%jdi=datenum('30-Jul-2011 15:34:44'); jdf=datenum('30-Jul-2011 17:25:30')
+%jdi=datenum('30-Jul-2011 21:57:47'); jdf=datenum('30-Jul-2011 23:59:15')
+%jdi=datenum('30-Jul-2011 23:50:44'); jdf=datenum('31-Jul-2011 05:15:37')
+%jdi=datenum('31-Jul-2011 06:48:47'); jdf=datenum('31-Jul-2011 11:01:12')
+%jdi=datenum('31-Jul-2011 21:21:58'); jdf=datenum('01-Aug-2011 00:03:47')
+%jdi=datenum('31-Jul-2011 23:59:22'); jdf=datenum('01-Aug-2011 04:44:44')
+%jdi=datenum('01-Aug-2011 17:21:22'); jdf=datenum('01-Aug-2011 17:46:05')
+%jdi=datenum('01-Aug-2011 21:04:40'); jdf=datenum('01-Aug-2011 23:20:08')
+%jdi=datenum('02-Aug-2011 00:24:54'); jdf=datenum('02-Aug-2011 01:16:22')
+%jdi=datenum('02-Aug-2011 07:17:37'); jdf=datenum('02-Aug-2011 09:51:12')
+%jdi=datenum('02-Aug-2011 22:06:26'); jdf=datenum('03-Aug-2011 00:00:05')
+%jdi=datenum('02-Aug-2011 23:59:22'); jdf=datenum('03-Aug-2011 06:01:44')
+%jdi=datenum('03-Aug-2011 05:59:22'); jdf=datenum('03-Aug-2011 09:04:15')
+%jdi=datenum('03-Aug-2011 23:09:01'); jdf=datenum('04-Aug-2011 00:00:54')
+%jdi=datenum('05-Aug-2011 07:28:19'); jdf=datenum('05-Aug-2011 08:33:22')
+%% DRY, NO RAIN, CLOUDS ABOVE 4KM
+%jdi=datenum('30-Jul-2011 01:26:17'); jdf=datenum('30-Jul-2011 06:00:31')
+%jdi=datenum('01-Aug-2011 04:55:56'); jdf=datenum('01-Aug-2011 06:02:17')
+%jdi=datenum('01-Aug-2011 05:59:07'); jdf=datenum('01-Aug-2011 11:02:17')
+%jdi=datenum('01-Aug-2011 17:59:49'); jdf=datenum('01-Aug-2011 20:49:14')
+%jdi=datenum('02-Aug-2011 01:24:10'); jdf=datenum('02-Aug-2011 06:02:17')
+%jdi=datenum('02-Aug-2011 06:06:10'); jdf=datenum('02-Aug-2011 06:59:28')
+%jdi=datenum('04-Aug-2011 17:20:17'); jdf=datenum('04-Aug-2011 18:02:38')
+%jdi=datenum('04-Aug-2011 17:57:00'); jdf=datenum('04-Aug-2011 23:28:45')
+%jdi=datenum('05-Aug-2011 02:32:38'); jdf=datenum('05-Aug-2011 05:43:35')
+%jdi=datenum('05-Aug-2011 05:58:45'); jdf=datenum('05-Aug-2011 07:27:00')
+
 
 %% FIRST DATE
-jdi0=datenum(2011, 7, 29, 0, 0, 0);
+jdi0=datenum(2011, 7, 29, 6, 0, 0);
 %jdi0=datenum(2011, 12, 14, 0, 0, 0);
 %jdi0=datenum(2011, 12, 21, 0, 0, 0);
 %jdi0=datenum(2012,  1, 19,12, 0, 0);
@@ -57,75 +91,36 @@ while (nday<=7)
     continue;
   end
   
-  %% SMOOTH OVER TIME
-  % running average of 5 minutes (+-2min)
-  % this is wrong! files may not be continous in time!!!
-  for ch=1:heads(1).nch
-    chphy(ch).tsm = smooth_time( chphy(ch).data , 2);
-  end
-  disp(['[2] time smoothing finished @ ']); toc
-  chphy=rmfield(chphy,'data');
-  
-  %% CROP TIME, i.e., KEEP ONLY 1 OUT OF 5 MINUTES
-  ncrop=0;
-  for nf=1:nfile
-    if mod(heads(nf).hourf(2), 5)==0
-      ncrop=ncrop+1;
-      heads_crop(ncrop) = heads(nf);
-      for ch=1:heads(1).nch
-	chphy(ch).crop(:,ncrop) = chphy(ch).tsm(:,nf);
-      end
-    end
-  end
-  disp(['[3] Cropping @ ']); toc
-  chphy=rmfield(chphy,'tsm');
-  
-  %% GLUE ANALOG+PC
-  chphy(6).crop=glue(chphy(1).crop, heads_crop(1).ch(1), ...
-		     chphy(2).crop, heads_crop(1).ch(2));
-  chphy(7).crop=glue(chphy(3).crop, heads_crop(1).ch(3), ...
-		     chphy(4).crop, heads_crop(1).ch(4));
-  disp(['[4] Glueing finished @ ']); toc
-  
-  %% SMOOTH IN THE VERTICAL
-  for ch=1:heads(1).nch+2
-    chphy(ch).vsm = ...
-	smooth_region( chphy(ch).crop , 3, 400, 7, 800, 10);
-  end
-  disp(['[5] vertical smoothing finished @ ']); toc
-  chphy=rmfield(chphy,'crop');
-  
   %% REMOVE BACK GROUND NOISE
   % average noise and stdev are calculated from last 500 bins
   % values below (bg+3*std) become zero
-  for ch=1:heads(1).nch+2
-    chphy(ch).cs = remove_bg(chphy(ch).vsm, 500, 3);
+  for ch=1:heads(1).nch
+    chphy(ch).cs = remove_bg(chphy(ch).data, 500, 3);
   end
   disp(['[6] bg noise finished @ ']); toc
-  chphy=rmfield(chphy,'vsm');
   
   %% RANGE CORRECTED SIGNAL
   for i=1:heads(1).ch(1).ndata
-    zh(i,1:ncrop)=(7.5*i);
+    zh(i,1:nfile)=(7.5*i);
   end
-  for ch=1:heads(1).nch+2
+  for ch=1:heads(1).nch
     chphy(ch).rcs = chphy(ch).cs .* zh .* zh;
   end
   disp(['[7] range corrected signal finished @ ']); toc
 
   %% PLOT
-  nslot=(jdf-jdi)*1440/5+1;
+  nslot=(jdf-jdi)*1440+1;
   data(1:2000,1:nslot)=NaN;
-  yy=((1:nslot)-1)*5/1440+jdi;
+  yy=((1:nslot)-1)/1440+jdi;
 
-  for i=1:ncrop
-    j=floor((heads_crop(i).jdi-jdi)*1440/5+0.5)+1;
-    data(:,j)=chphy(6).rcs(1:2000,i);
+  for i=1:nfile
+    j=floor((heads(i).jdi-jdi)*1440+0.5)+2;
+    data(:,j)=chphy(1).rcs(1:2000,i);
   end
   
   figure(1)
   set(gcf,'position',[0,500,1400,600]); % units in pixels!
-  gplot2(data,[0:1.e7:1.5e9],yy,zh(1:2000,1)/1e3)
+  gplot2(data,[0:3e5:3e7],yy,zh(1:2000,1)/1e3)
   datetick('x','mm/dd HH:MM')
   ylabel('Altitude agl (km)')
   tmp=datevec(jdi);
