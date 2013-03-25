2a4413383105c760656282200016fe72fa8a47e7
Henrique Barbosa <hmjbarbosa@gmail.com>
2013-03-25 19:44:34 -0300

    fixing bug in gplot2. now it verify the size of x, y and mat2d.

--- matlab/gplot2.m
+++ matlab/gplot2.m
@@ -14,17 +14,23 @@ end
 
 if ~exist('X','var') X=[1:nt]; end;
 if ~exist('Y','var') Y=[1:nz]; end;
+if (size(mat2d,2)~=nt)
+  disp('Error: X and mat2d must have the same number of columns');
+  return
+end
+if (size(mat2d,1)~=nz)
+  disp('Error: Y and mat2d must have the same number of lines');
+  return
+end
 
 if ~exist('clev','var') || numel(clev)==0
-  q=quantile(nanmax(mat2d), [0.5 0.75]);
+  q=quantile(nanmax(mat2d), [0.5 0.95]);
   vmax=q(2);
   dv=vmax/50.;
   clev=[0:dv:vmax];
 end
 [cmap, clim]=cmapclim(clev);
 
-%[C, h]=contourf(X, Y, mat2d,clev);
-%set(h,'edgecolor','none');
 imsc(X, Y, mat2d,clim,cmap,[1 1 1]);
 set(gca,'YDir','normal');
 
