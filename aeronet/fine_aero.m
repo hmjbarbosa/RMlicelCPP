aae2b5f46da86afd76189aa4880824bf945f441e
Henrique <hbarbosa@samsung>
2013-03-28 09:15:57 -0300

    more aeronet plots.

--- aeronet/fine_aero.m
+++ aeronet/fine_aero.m
@@ -11,82 +11,49 @@ xlist=2011+((1:nlist)-0.5)*groupday/365.25;
 
 % ---- total
 figure(1)
-set(gcf,'PaperUnits','inches','PaperPosition',[0 0 8 3])
-sub=subplot('position',[0.08 0.13 0.65 0.78]);
-plot(aero.jd,aero.aot_total,'.');
-datetick('x','yy/mm');
-ylim([0 1]); 
-title('Manaus-AM/EMBRAPA','fontsize',[14]);
-xlabel('Years','fontsize',[12])  
-ylabel('Total AOD 500nm','fontsize',[12])
-grid on;
-sub=subplot('position',[0.8 0.13 0.15 0.78]);
-bins=[0:0.025:1];
-counts=histc(aero.aot_total(:,1),bins);
-b=barh(bins+bins(2)/2,counts/sum(counts),1,'w'); 
-set(b,'facecolor',[0.7 0.7 0.7]);
-ylim([0 1]); xlim([0, 0.5]); xlabel('freq');
-set(gca,'XTick',[0 .1 .2 .3 .4 .5]);
-set(gca,'xticklabel',' |.1|.2|.3|.4|.5');
-set(gca,'yticklabel','');
-grid on;
-print('sda_time_aod_total.png','-dpng');
-%!mogrify -trim sda_time_aod_total.png
+fine_aero_plot(aero.jd, aero.aot_total, 'Total AOD 500nm',...
+               [0 .1 .2 .3],' |.1|.2|.3' )
+print(['sda_time_aod_total.png'],'-dpng');
 
-return
+figure(5)
+fine_aero_box(aero.jd, aero.aot_total(:,1), 'Total AOD 500nm',...
+               [0 .1 .2 .3],' |.1|.2|.3' )
+print(['sda_timebox_aod_total.png'],'-dpng');
+
 % ---- fine
 figure(2)
-set(gcf,'PaperUnits','inches','PaperPosition',[0 0 8 3])
-plot(time,aero.aot_fine,'.');
-ylim([0 1]); 
-title('Manaus-AM/EMBRAPA','fontsize',[14]);
-xlabel('Years','fontsize',[12])  
-ylabel('Fine AOD 500nm','fontsize',[12])
-grid on;
+fine_aero_plot(aero.jd, aero.aot_fine, 'Fine AOD 500nm',...
+               [0 .1 .2 .3 .4],' |.1|.2|.3|.4' )
 print('sda_time_aod_fine.png','-dpng');
-!mogrify -trim sda_time_aod_fine.png
+
+figure(6)
+fine_aero_box(aero.jd, aero.aot_fine(:,1), 'Fine AOD 500nm',...
+               [0 .1 .2 .3 .4],' |.1|.2|.3|.4' )
+print(['sda_timebox_aod_fine.png'],'-dpng');
+
 % ---- coarse
 figure(3)
-set(gcf,'PaperUnits','inches','PaperPosition',[0 0 8 3])
-plot(time,aero.aot_coarse,'.');
-ylim([0 1]); 
-title('Manaus-AM/EMBRAPA','fontsize',[14]);
-xlabel('Years','fontsize',[12])  
-ylabel('Coarse AOD 500nm','fontsize',[12])
-grid on;
+fine_aero_plot(aero.jd, aero.aot_coarse, 'Coarse AOD 500nm',...
+               [0 .15 .3 .45 ],' |.15|.30|.45' )
 print('sda_time_aod_coarse.png','-dpng');
-!mogrify -trim sda_time_aod_coarse.png
+
+figure(7)
+fine_aero_box(aero.jd, aero.aot_coarse(:,1), 'Coarse AOD 500nm',...
+               [0 .15 .3 .45 ],' |.15|.30|.45' )
+print(['sda_timebox_aod_coarse.png'],'-dpng');
+
 % ---- finefrac
 figure(4)
-set(gcf,'PaperUnits','inches','PaperPosition',[0 0 8 3])
-plot(time,aero.aot_finefrac,'.');
-ylim([0 1]); 
-title('Manaus-AM/EMBRAPA','fontsize',[14]);
-xlabel('Years','fontsize',[12])  
-ylabel('Finefrac AOD 500nm','fontsize',[12])
-grid on;
+fine_aero_plot(aero.jd, aero.aot_finefrac, 'Finefrac AOD 500nm',...
+               [0 .05 .1 .15],' |.05|.10|.15' )
 print('sda_time_aod_finefrac.png','-dpng');
-!mogrify -trim sda_time_aod_finefrac.png
 
-%% AOD em 500nm
+figure(8)
+fine_aero_box(aero.jd, aero.aot_finefrac(:,1), 'Finefrac AOD 500nm',...
+               [0 .05 .1 .15],' |.05|.10|.15' )
+print(['sda_timebox_aod_finefrac.png'],'-dpng');
 
-X_tot(1:aero.ntimes,1:nlist)=NaN;
-X_fin(1:aero.ntimes,1:nlist)=NaN;
-X_coa(1:aero.ntimes,1:nlist)=NaN;
-X_fra(1:aero.ntimes,1:nlist)=NaN;
-for i=1:nlist
-  for j=1:aero.ntimes
-    if list(j)==i
-      % column 1 is value
-      % column 2 is error
-      X_tot(j,i)=aero.aot_total(j,1);   
-      X_fin(j,i)=aero.aot_fine(j,1);     
-      X_coa(j,i)=aero.aot_coarse(j,1);   
-      X_fra(j,i)=aero.aot_finefrac(j,1); 
-    end
-  end
-end
-
+return
 nn=8;
 % ---- total
 figure(5)
@@ -102,6 +69,7 @@ ylabel('Total AOD 500nm','fontsize',[12])
 grid on;
 print('sda_timebox_aod_total.png','-dpng');
 !mogrify -trim sda_timebox_aod_total.png
+return
 % ---- fine
 figure(6)
 set(gcf,'PaperUnits','inches','PaperPosition',[0 0 8 3])
