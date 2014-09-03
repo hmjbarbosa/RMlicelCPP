d3ede45cbb24056b3b61605a649e2d36540a9857
Henrique de Melo Jorge Barbosa <hmj.barbosa+git@gmail.com>
2014-09-02 23:38:02 -0300

    New constants.m that now creates a matlab object with all the information inside. CO2 value is fixed at 400ppm.

--- leipzig_birgit/constants.m
+++ leipzig_birgit/constants.m
@@ -1,4 +1,4 @@
-%------------------------------------------------------------------------
+%
 % M-File:
 %    molecular.m
 %
@@ -9,10 +9,6 @@
 %
 %    Defines important physic's constants, most of which are used
 %    by the molecular scattering calculation.
-%
-% Input
-%
-%    co2ppmv - CO2 concentration in ppmv
 %
 % Ouput
 %
@@ -39,74 +35,110 @@
 %    Wallace and Hobbs, Atmospheric science (2nd Edition)
 %    Bodhaine et al, 1999: J. Atmos. Ocea. Tech, v. 16, p.1854
 %
-%------------------------------------------------------------------------
 
-clear Airmwt Armwt Arppv CO2mwt CO2ppv H2mwt H2ppv Hemwt ...
-    Heppv Krmwt Krppv Mvol N2mwt N2ppv Na Nemwt Neppv Nstd ...
-    O2mwt O2ppv Pstd Rair Rgas T0 Tstd Xemwt Xeppv k
-
-%------------------------------------------------------------------------
-% User definitions (should be done before calling the routine)
-%------------------------------------------------------------------------
-% co2ppmv = 392; % CO2 concentration [ppmv]
-
-disp(['constants:: co2ppmv = ' num2str(co2ppmv)]);
+clear cte
+j=0;
 
 %------------------------------------------------------------------------
 % Fixed definitions
 %------------------------------------------------------------------------
 
 % Atmospheric Constituents Concentration
-% units: ppv
 % ref: Seinfeld and Pandis (1998)
-N2ppv=(1e-2)*78.084;
-O2ppv=(1e-2)*20.946;
-Arppv=(1e-2)*0.934;
-Neppv=(1e-2)*1.80*1e-3;
-Heppv=(1e-2)*5.20*1e-4;
-Krppv=(1e-2)*1.10*1e-4;
-H2ppv=(1e-2)*5.80*1e-5;
-Xeppv=(1e-2)*9.00*1e-6;
-CO2ppv=co2ppmv*1e-6;
-
+j=j+1;
+cte.units{j,1}='gas';
+cte.units{j,2}='Name';
+cte.units{j,3}='[]';
+j=j+1;
+cte.units{j,1}='ppv';
+cte.units{j,2}='Concentration';
+cte.units{j,3}='[ppv]';
 % Atmospheric Constituents Molecular weight
-% units: grams per mol
 % ref: Handbook of Physics and Chemistry (CRC 1997)
-N2mwt=28.013;
-O2mwt=31.999;
-Armwt=39.948;
-Nemwt=20.18;
-Hemwt=4.003;
-Krmwt=83.8;
-H2mwt=2.016;
-Xemwt=131.29;
-CO2mwt=44.01;
+j=j+1;
+cte.units{j,1}='mwt';
+cte.units{j,2}='Molecular weight';
+cte.units{j,3}='[kg / mol]';
+%
+cte.gas{1}='N2';  cte.ppv(1)=0.78084;  cte.mwt(1)=28.013e-3;
+cte.gas{2}='O2';  cte.ppv(2)=0.20946;  cte.mwt(2)=31.999e-3;
+cte.gas{3}='Ar';  cte.ppv(3)=0.00934;  cte.mwt(3)=39.948e-3;
+cte.gas{4}='Ne';  cte.ppv(4)=1.80e-5;  cte.mwt(4)=20.180e-3;
+cte.gas{5}='He';  cte.ppv(5)=5.20e-6;  cte.mwt(5)=4.0030e-3;
+cte.gas{6}='Kr';  cte.ppv(6)=1.10e-6;  cte.mwt(6)=83.800e-3;
+cte.gas{7}='H2';  cte.ppv(7)=5.80e-7;  cte.mwt(7)=2.0160e-3;
+cte.gas{8}='Xe';  cte.ppv(8)=9.00e-8;  cte.mwt(8)=131.29e-3;
+cte.gas{9}='CO2'; cte.ppv(9)=4.00e-4;  cte.mwt(9)=44.010e-3; 
 
-% Dry air molecular mass (grams per mol)
-Airmwt=(N2ppv*N2mwt + O2ppv*O2mwt + Arppv*Armwt + Neppv*Nemwt + ...
-	Heppv*Hemwt + Krppv*Krmwt + H2ppv*H2mwt + Xeppv*Xemwt + ...
-	CO2ppv*CO2mwt) / (N2ppv + O2ppv + Arppv + Neppv + Heppv + ...
-			  Krppv + H2ppv + Xeppv + CO2ppv);
+% force normalization depending of CO2 concentration
+cte.ppv=cte.ppv/sum(cte.ppv);
+
+% Now add dry air
+cte.gas{10}='Air';
+cte.mwt(10)=sum(cte.ppv.*cte.mwt)/sum(cte.ppv);
+cte.ppv(10)=sum(cte.ppv);
 
 % Physics
-k=1.3806503e-23; % Boltzmann [J / K]
-Na=6.0221367e23; % Avogadro's number [#/mol]
+j=j+1;
+cte.units{j,1}='k';
+cte.units{j,2}='Boltzmann constant';
+cte.units{j,3}='[J / K]';
+cte.k=1.3806503e-23;
+
+j=j+1;
+cte.units{j,1}='Na';
+cte.units{j,2}='Avogadro number';
+cte.units{j,3}='[# / mol]';
+cte.Na=6.0221367e23;
 
 % Wallace and Hobbs, p. 65
-Rgas=k*Na;            % Universal gas constant [J / K / mol]
-Rair=Rgas/Airmwt*1e3; % Dry air gas constant [J / K / kg]
+j=j+1;
+cte.units{j,1}='Rgas';
+cte.units{j,2}='Universal gas constant';
+cte.units{j,3}='[J / K / mol]';
+cte.Rgas=cte.k*cte.Na;
+
+j=j+1;
+cte.units{j,1}='Rair';
+cte.units{j,2}='Dry air gas constant';
+cte.units{j,3}='[J / K / kg]';
+cte.Rair=cte.Rgas/cte.mwt(10);
 
 % Standard Atmosphere Reference values
-T0=273.15; % zero deg celcius [K]
-Tstd=288.15;  % Temperature [K]
-Pstd=101325.; % Pressure [Pa]
+j=j+1;
+cte.units{j,1}='T0';
+cte.units{j,2}='Water freezing point';
+cte.units{j,3}='[K]';
+cte.T0=273.15;
 
-% Molar volume [m3/mol] at Pstd and T0
-% Bodhaine et al, 1999
-Mvol=22.4141e-3;
+j=j+1;
+cte.units{j,1}='Tstd';
+cte.units{j,2}='Standard temperature';
+cte.units{j,3}='[K]';
+cte.Tstd=288.15;
 
-% Molecular density [#/m3] at Tstd and Pstd
-Nstd=(Na/Mvol)*(T0/Tstd); 
-disp(['constants:: Nstd = ' num2str(Nstd) ' #/m3']);
+j=j+1;
+cte.units{j,1}='Pstd';
+cte.units{j,2}='Standard pressure';
+cte.units{j,3}='[Pa]';
+cte.Pstd=101325.;
 
+% Bodhaine et al, 1999, but at Tstd and Pstd
+j=j+1;
+cte.units{j,1}='Mvol';
+cte.units{j,2}='Standard molar volume';
+cte.units{j,3}='[m3 / mol]';
+cte.Mvol=22.4141e-3*cte.Tstd/cte.T0;
+
+% Bodhaine et al, 1999, at Tstd and Pstd
+j=j+1;
+cte.units{j,1}='Nstd';
+cte.units{j,2}='Standard molecular density';
+cte.units{j,3}='[# / m3]';
+cte.Nstd=cte.Na/cte.Mvol; 
+
+%
+cte
+[[cte.units{1,:} cte.gas]', [cte.units{2,:} num2cell(cte.ppv)]', [cte.units{3,:} num2cell(cte.mwt)]']
+cte.units
 %
