672068f24a7a1e12213edd6348f1e9e8b866d650
Henrique de Melo Jorge Barbosa <hmj.barbosa+git@gmail.com>
2015-04-22 10:25:52 -0300

    Fixing units, and changing synthetic() to a function.

--- leipzig_birgit/read_sonde_synthetic.m
+++ leipzig_birgit/read_sonde_synthetic.m
@@ -30,62 +30,97 @@
 %    Then execute this script.
 %
 %------------------------------------------------------------------------
-clear sonde radiofile
-clear temp_snd pres_snd rho_snd alt_snd nlev_snd
+function [snd] = read_sonde_synthetic(radiofile, debug)
 
-% This file cannot be read with constant width because the columns
-% shift left/right. This happens because there is only a single space
-% between each column, and the number of digits in each column
-% varies. Hence, we read as a numeric table with space as column
-% separator
-radiofile=['../synthetic_signals/Temperature_and_Pressure/txt/PTsim.txt.txt']
-disp(['*** read radiosounding data ' radiofile]);
-sonde=importdata(radiofile, ' ', 1);
+T0=273.15; % K
+Rair=287.0417; % J/K/kg, for 400 ppmv CO2
 
-pres_snd=sonde.data(:,3);  % P in hPa!
-alt_snd=sonde.data(:,2); % in m 
-temp_snd=T0 + sonde.data(:,4); % T in K
+% open input file
+snd.fname=radiofile;
+if ~exist('radiofile','var')
+  error('File name not given. Try: help read_sonde');
+else
+  % This file cannot be read with constant width because the columns
+  % shift left/right. This happens because there is only a single space
+  % between each column, and the number of digits in each column
+  % varies. Hence, we read as a numeric table with space as column
+  % separator
 
-% P = rho*R*T, Rair=287.05 J/kg/K
-% 100 corrects hPa to Pa, hence rho in kg/m3
-rho_snd=100*pres_snd./temp_snd/Rair;
+  sonde=importdata(snd.fname, ' ', 1);
+end
+
+% set default debug level
+if ~exist('debug','var')
+  debug=0;
+end
+
+if (debug>0)
+  disp(['read_sonde:: input file = ' radiofile]);
+end
+
+snd.code='99999';
+snd.station='fake';
+snd.jd=datenum('0:00 1 jan 1980');
+snd.date=datevec(snd.jd);
+
+snd.pres(:,1)=sonde.data(:,3)*100.;  % P in Pa!
+snd.alt(:,1)=sonde.data(:,2); % in m 
+snd.temp(:,1)=T0 + sonde.data(:,4); % T in K
+
+% compute air density
+% P = rho*R*T, R=287.05 J/kg/K
+% rho in kg/m3
+snd.rho(:,1)=snd.pres./snd.temp/Rair;
 
 % number of levels in sounding
-nlev_snd=max(size(pres_snd));
+snd.nlev=max(size(snd.pres));
+
+if (debug>0)
+  disp(['read_sonde:: nlev_snd = ' num2str(snd.nlev)]);
+  disp(['read_sonde:: highest = ' ...
+	num2str(snd.pres(snd.nlev)) ' Pa / ' ... 
+	num2str(snd.alt(snd.nlev)) ' m ']);
+end
 
 %------------------------------------------------------------------------
 %  Plots
 %------------------------------------------------------------------------
-if (debug<1)
-  return
+%
+if (debug>1)
+  figure
+  temp=get(gcf,'position'); temp(3)=260; temp(4)=650;
+  set(gcf,'position',temp); % units in pixels!
+  plot(snd.temp,snd.alt*1e-3,'Color','r','linewidth',1.5);
+  hold on
+  ax1 = gca;
+  set(ax1,'XColor','r','YColor','k','XAxisLocation','bottom')
+  ylabel(ax1,'Height / km')
+  xlabel(ax1,'Temperature / K')
+  xlimits = get(ax1,'XLim');
+  ylimits = get(ax1,'YLim');
+  xinc = (xlimits(2)-xlimits(1))/5;
+  yinc = (ylimits(2)-ylimits(1))/5;
+  
+  set(ax1,'XTick',[xlimits(1):xinc:xlimits(2)],...
+	  'YTick',[ylimits(1):yinc:ylimits(2)]);
+  
+  ax2 = axes('Position',get(ax1,'Position'),'XAxisLocation','top',...
+	     'YAxisLocation','right','Color','none',...
+	     'XColor','b','YColor','k');
+  
+  xlabel(ax2,'density / kg/m3')
+  line(snd.rho,snd.alt*1e-3,'Color','b','Parent',ax2,'linewidth',1.5);
+  xlimits = get(ax2,'XLim');
+  xinc = (xlimits(2)-xlimits(1))/5;
+  set(ax2,'XTick',[xlimits(1):xinc:xlimits(2)],...
+	  'YTick',[ylimits(1):yinc:ylimits(2)]);
+  
+  grid on
+  hold off
 end
-%
-%
-% -------------
-figure(4)
-xx=xx0+5*wdx; yy=yy0+5*wdy;
-set(gcf,'position',[xx,yy,wsx,wsy]); % units in pixels!
-plot(temp_snd,alt_snd*1e-3,'Color','r');
-hold on
-ax1 = gca;
-set(ax1,'XColor','r','YColor','k','XAxisLocation','bottom')
-ylabel(ax1,'Height / km')
-xlabel(ax1,'Temperature / K')
-xlimits = get(ax1,'XLim')
-ylimits = get(ax1,'YLim');
-xinc = (xlimits(2)-xlimits(1))/5;
-yinc = (ylimits(2)-ylimits(1))/5;
-
-set(ax1,'XTick',[xlimits(1):xinc:xlimits(2)],...
-        'YTick',[ylimits(1):yinc:ylimits(2)]);
-
-ax2 = axes('Position',get(ax1,'Position'),'XAxisLocation','top',...
-           'YAxisLocation','right','Color','none',...
-           'XColor','b','YColor','k');
-xlabel(ax2,'density / kg/m3')
-line(rho_snd,alt_snd,'Color','b','Parent',ax2);
-grid on
-hold off
+set(gcf,'PaperUnits','inches','PaperSize',[3,9],'PaperPosition',[0 0 3 7.8]);
+%prettify(gca); grid on;
+print('earlinet_PT.png','-dpng');
 %
 %
 disp('End of program: read_sonde_synthetic.m, Vers. 1.0 06/2012')
