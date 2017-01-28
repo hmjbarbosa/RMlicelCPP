6c7cecf024dc59de63a50746f47007e578dd0b10
Henrique de Melo Jorge Barbosa <hmj.barbosa+git@gmail.com>
2017-01-28 12:18:01 -0200

    better reading routine for Bolivia

--- leipzig_birgit/lalinet201701/profile_read_ascii_Bolivia.m
+++ leipzig_birgit/lalinet201701/profile_read_ascii_Bolivia.m
@@ -41,120 +41,95 @@ if isempty(ach) ach=0; end
 if ~exist('maxz','var') maxz=0; end
 if isempty(maxz) maxz=0; end
 
-% bolivia does not use LICEL, we have to invent some fake header info
-[PATHSTR,NAME,EXT] = fileparts(fname);
-head.file=[NAME '.' EXT];
-
-% site name is not defined
-head.site='Bolivia'; 
-
-% get date from filename
-head.datei=[str2num(NAME(1:4)), str2num(NAME(6:7)), str2num(NAME(9:10))];
-head.datef=head.datei;
-
 %% OPEN FILE
 fp=fopen(fname,'r');
 if (fp < 0)
   error(['fail to open file: ' fname]);
+else
+  disp(['File= ' fname]);
 end
 % find out the number of columns
 tmp=fgets(fp);
-tmp=strfind(tmp,sprintf('\t'));
-ncol=length(tmp)+1;
-% find number of lines
+colidx=strfind(tmp,sprintf('\t'));
+ncol=length(colidx)+1;
+disp(['Number of cols= ' num2str(ncol)]);
+if maxz==0
+  maxz=ncol-4;
+end
+fclose(fp);
 
-%fclose(fp)
+% find number of lines
+[status cmdout]=system(['wc -l ' fname]);
+scanCell = textscan(cmdout,'%u %s');
+lineCount = scanCell{1};
+disp(['Number of lines= ' num2str(lineCount)]);
 
 %% READ ALL DATA
-
-%fp=fopen(fname,'r');
-i=1;
-while ~isempty(tmp)
-    data(i,:)=fscanf(fp,'%f',ncol);
-    tmp=fgets(fp); i=i+1
-end
+disp(['Reading data...']);
+tic
+fp=fopen(fname,'r');
+%data=fscanf(fp,'%f',[ncol inf]);
+data=fscanf(fp,'%f',[ncol lineCount]);
 fclose(fp);
-phy=data;
-return
+toc
 
-head.datei=fscanf(fp,'%2d/%2d/%4d',[1,3]); %% DD MM YY
-head.houri=fscanf(fp,'%2d:%2d:%2d',[1,3]); %% hh mn ss
-head.datef=fscanf(fp,'%2d/%2d/%4d',[1,3]); %% DD MM YY
-head.hourf=fscanf(fp,'%2d:%2d:%2d',[1,3]); %% hh mn ss
-head.jdi=datenum([head.datei(3:-1:1) head.houri]);
-head.jdf=datenum([head.datef(3:-1:1) head.hourf]);
+%% CREATE HEADER
+disp(['Creating header...']);
+% bolivia does not use LICEL, we have to invent some fake header info
+[PATHSTR,NAME,EXT] = fileparts(fname);
+% get date from filename
+datei=[str2num(NAME(9:10)), str2num(NAME(6:7)), str2num(NAME(1:4))];
+datef=datei;
+% get time from first 3 columns
+timei=data(1:3,:)';
 
-head.alt=fscanf(fp,'%d',[1,1]);
-lon=fscanf(fp,'%s',[1,1]);  lon(lon==',')='.'; head.lon=str2num(lon);    
-lat=fscanf(fp,'%s',[1,1]); lat(lat==',')='.'; head.lat=str2num(lat);
-% Some old Licel does not include 00, T0 and P0 in this line. So we
-% need to read the rest of the line, and from that try to read what we
-% want. 
-tmp=fgets(fp);
-[A,n]=sscanf(tmp,'%s %s %s %s');
-if n>=1
-  zen=A(1); zen(zen==',')='.'; head.zen=str2num(zen);    
-end
-if n>=2
-  idum=A(2); idum(idum==',')='.'; head.idum=str2num(idum);
-end
-if n>=3
-  T0=A(3); T0(T0==',')='.'; head.T0=str2num(T0);
-end
-if n>=4
-  P0=A(4); P0(P0==',')='.'; head.P0=str2num(P0);        
-end
+for i=1:lineCount
+  head(i).file=[NAME '.' EXT];
+  head(i).site='Bolivia'; 
 
-%% LINE #3
-head.nshoots=fscanf(fp,'%d',[1,1]);
-head.nhz=fscanf(fp,'%d',[1,1]);
-head.nshoots2=fscanf(fp,'%d',[1,1]);
-head.nhz2=fscanf(fp,'%d',[1,1]);
-head.nch=fscanf(fp,'%d',[1,1]);
+  head(i).datei=datei;
+  head(i).datef=datei;
 
-%% READ CHANNEL LINES
-for i = 1:head.nch
-  head.ch(i).active =fscanf(fp,'%d',[1,1]);             
-  head.ch(i).photons=fscanf(fp,'%d',[1,1]);             
-  head.ch(i).elastic=fscanf(fp,'%d',[1,1]);             
-  head.ch(i).ndata  =fscanf(fp,'%d 1',[1,1]);           
-  head.ch(i).pmtv   =fscanf(fp,'%d',[1,1]);             
-  head.ch(i).binw   =fscanf(fp,'%f',[1,1]);             
-  head.ch(i).wlen   =fscanf(fp,'%5d.',[1,1]);            
-  head.ch(i).pol    =fscanf(fp,'%1c 0 0 00 000 ',[1,1]);
-  head.ch(i).bits   =fscanf(fp,'%d',[1,1]);             
-  head.ch(i).nshoots=fscanf(fp,'%d',[1,1]);             
-  head.ch(i).discr  =fscanf(fp,'%f',[1,1]);             
-  head.ch(i).tr     ={fscanf(fp,'%s',[1,1])};
-end
+  head(i).houri=timei(i,:);
+  head(i).hourf=timei(i,:);
+  
+  head(i).jdi=datenum([head(i).datei(3:-1:1) head(i).houri]);
+  head(i).jdf=head(i).jdi;
+  
+  head(i).alt=3420;
+  head(i).lon=-68;
+  head(i).lat=-16.5;
+  head(i).zen=0;
+  head(i).idum=0;
+  head(i).T0=23;
+  head(i).P0=1013;
 
-% jump extra line
+  head(i).nshoots=3;
+  head(i).nhz=10;
+  head(i).nshoots2=head(i).nshoots;
+  head(i).nhz2=head(i).nhz;
+  head(i).nch=1;
 
-tmp=fgets(fp);
+  % channel info
+  for j = 1:head(i).nch
+    head(i).ch(j).active =1;
+    head(i).ch(j).photons=0;
+    head(i).ch(j).elastic=1;
+    head(i).ch(j).ndata  =min(ncol-4, maxz);
+    head(i).ch(j).pmtv   =0;
+    head(i).ch(j).binw   =data(4,i)*3e8; 
+    head(i).ch(j).wlen   =532;
+    head(i).ch(j).pol    ='o';
+    head(i).ch(j).bits   =0;
+    head(i).ch(j).nshoots=3;
+    head(i).ch(j).discr  =0;
+    head(i).ch(j).tr     =0;
+  end
+  
+end  
 
-% takes 0.3 sec for 100 files
-% close to 0.07 for reading the binary files
-phy=fscanf(fp,'%f',[head.nch head.ch(1).ndata])';
+phy=data(5:4+head(1).ch(1).ndata , : );
 
-fclose(fp);
 
-% copy to final destination
-nz=head.ch(1).ndata;
-if (maxz==0)
-    maxz=nz;
-else
-    maxz=min(nz, maxz);
-    for ch=1:head.nch
-        head.ch(ch).ndata=maxz;
-    end
-end
-
-if (ach==0) 
-    phy = phy(1:maxz,:);
-else
-    phy = phy(1:maxz,ach);
-    head.ch = head.ch(ach);
-    head.nch = 1;
-end
 
 %%
